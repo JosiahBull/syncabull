21e133d5c32196d676ce7857c0a28fd62522362a
Josiah Bull <josiah.bull7@gmail.com>
2022-11-02 13:52:42 +1300

    feat: tidied up the homepage

--- app/components/Nav.vue
+++ app/components/Nav.vue
@@ -1,6 +1,11 @@
 <template>
 <div>
-  <b-navbar toggleable="lg" type="light" variant="light" class="border-bottom">
+  <b-navbar
+    toggleable="lg"
+    type="light"
+    variant="light"
+    class="border-bottom emphasis"
+  >
     <b-navbar-brand href="/">Syncabull</b-navbar-brand>
     <b-navbar-nav>
       <b-nav-item href="https://github.com/JosiahBull/syncabull/"><b-icon icon="github" font-scale="1.5" class="mr-1"></b-icon> Repository </b-nav-item>
@@ -19,3 +24,13 @@ export default {
   name: 'Nav',
 }
 </script>
+
+<style scoped>
+
+.emphasis {
+  /* add some emphasis to the bottom of the navbar, so that it becomes distinct against the white-on-white background */
+  box-shadow: 0 0.25rem 0.75rem rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: 0 0.25rem 0.75rem rgba(0, 0, 0, 0.3);
+  -moz-box-shadow: 0 0.25rem 0.75rem rgba(0, 0, 0, 0.3);
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
+
     <Head></Head>
     <Nav id="nav"></Nav>
+
     <b-container id="hero" class="slide-up-fade-in">
       <b-row>
         <h1 class="mx-auto align-middle text-center font-weight-bolder sans">Backup Your <br>Google Photos</h1>
@@ -10,28 +12,138 @@
         <LoginButton class="mx-auto align-middle font-weight-bold"></LoginButton>
       </b-row>
     </b-container>
+
+    <FloatingBoxes></FloatingBoxes>
+
+    <!-- Information about the application -->
+    <!-- There is an arrow with "more info" at the bottom of the landing page  -->
+    <!-- 3x2 grid of cards, each containing an image, title, and short description -->
+
+    <b-container id="info">
+
+      <b-col class="mx-auto align-middle text-center clickable" @click="onClickScroll('#info')">
+        <p class="mx-auto align-middle text-center text-muted m-0 p-0">More</p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="text-muted mb-5"
+          viewBox="0 0 16 16">
+          <path fill-rule="evenodd"
+            d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
+        </svg>
+      </b-col>
+      <!-- Title -->
+      <b-row>
+        <h2 class="mx-auto align-middle text-center light">Why Backup Your Photos?</h2>
+      </b-row>
+
+      <!-- 3x2 grid of cards, each containing an image, title, and short description -->
+      <span>
+        <b-row>
+          <b-col cols="12" md="4" class="mb-4">
+            <b-card class="h-100">
+              <b-card-img src="https://picsum.photos/300/200?image=0" alt="Image 0"></b-card-img>
+              <b-card-body>
+                <b-card-title>Open Source</b-card-title>
+                <b-card-text>
+                  This application is open source, and always will be.
+                </b-card-text>
+              </b-card-body>
+            </b-card>
+          </b-col>
+
+          <b-col cols="12" md="4" class="mb-4">
+            <b-card class="h-100">
+              <b-card-img src="https://picsum.photos/300/200?image=1" alt="Image 1"></b-card-img>
+              <b-card-body>
+                <b-card-title>Secure, and Encrypted</b-card-title>
+                <b-card-text>
+                  Your files are encrypted while in transit, and aren't stored on our servers.
+                </b-card-text>
+              </b-card-body>
+            </b-card>
+          </b-col>
+
+          <b-col cols="12" md="4" class="mb-4">
+            <b-card class="h-100">
+              <b-card-img src="https://picsum.photos/300/200?image=2" alt="Image 2"></b-card-img>
+              <b-card-body>
+                <b-card-title>Self Hostable</b-card-title>
+                <b-card-text>
+                  You can host this application yourself, and have full control over your data.
+                </b-card-text>
+              </b-card-body>
+            </b-card>
+          </b-col>
+        </b-row>
+
+        <b-row>
+          <b-col cols="12" md="4" class="mb-4">
+            <b-card class="h-100">
+              <b-card-img src="https://picsum.photos/300/200?image=0" alt="Image 0"></b-card-img>
+              <b-card-body>
+                <b-card-title>Powered by Rust and Docker</b-card-title>
+                <b-card-text>
+                  This application is built with Rust, and Docker. Blazing fast, secure, and reliable.
+                </b-card-text>
+              </b-card-body>
+            </b-card>
+          </b-col>
+
+          <b-col cols="12" md="4" class="mb-4">
+            <b-card class="h-100">
+              <b-card-img src="https://picsum.photos/300/200?image=1" alt="Image 1"></b-card-img>
+              <b-card-body>
+                <b-card-title>Fast and Stable</b-card-title>
+                <b-card-text>
+                  We have committed to a thorough testing process to ensure high levels of uptime. Your backups will always be safe.
+                </b-card-text>
+              </b-card-body>
+            </b-card>
+          </b-col>
+
+          <b-col cols="12" md="4" class="mb-4">
+            <b-card class="h-100">
+              <b-card-img src="https://picsum.photos/300/200?image=2" alt="Image 2"></b-card-img>
+              <b-card-body>
+                <b-card-title>Filter Duplicates</b-card-title>
+                <b-card-text>
+                  We won't store duplicate images in your backup. We'll only store the highest quality version of each image.
+                </b-card-text>
+              </b-card-body>
+            </b-card>
+          </b-col>
+        </b-row>
+      </span>
+
+    </b-container>
+
     <Footer></Footer>
   </div>
 </template>
 
-<script lang="ts">
+<script>
+import FloatingBoxes from '~/components/FloatingBoxes.vue';
 import Vue from 'vue'
 import LoginButton from '~/components/LoginButton.vue';
 
-function vert_cent_hero(element: any) {
+function vertCentHero(element) {
   const window_height = window.innerHeight;
-  const hero: any = document.querySelector('#hero');
+  const hero = document.querySelector('#hero');
   element.style.marginTop = (window_height / 2) - element.offsetHeight + 'px';
 }
 
 export default Vue.extend({
-    name: "IndexPage",
-    beforeMount() {
-        const el = document.querySelector("#hero");
-        vert_cent_hero(el);
-        window.addEventListener("resize", vert_cent_hero.bind(null, el));
-    },
-    components: { LoginButton }
+  name: "IndexPage",
+  beforeMount() {
+    const el = document.querySelector("#hero");
+    vertCentHero(el);
+    window.addEventListener("resize", vertCentHero.bind(null, el));
+  },
+  components: { LoginButton, FloatingBoxes },
+  methods: {
+    onClickScroll: function (element) {
+      const el = document.querySelector(element);
+      el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 })
 
 </script>
@@ -40,6 +152,20 @@ export default Vue.extend({
 #hero {
   margin-top: calc(50vh - 150px);
   opacity: 0;
+  /* upwards box shadow to differentiate */
+}
+
+#hero h1 {
+  font-size: 3rem;
+}
+
+#info {
+  /* start 50vh + 150px */
+  margin-top: calc(40vh);
+}
+
+.clickable {
+  cursor: pointer;
 }
 
 .outer-square {
@@ -163,7 +289,6 @@ p {
     -ms-transform: translate(0px, 0px);
   }
 }
-
 
 .clearfix {
   clear: both;
